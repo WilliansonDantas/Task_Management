ffa11c4ad4bb2f77c4b4b8c0c38938a8b424cae7
WilliansonDantas <williansondantas@gmail.com>
2024-09-24 19:31:52 -0300

    feat: create user task

---
+++ task-management-api/src/entities/task.entity.ts
@@ -0,0 +1,47 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { User } from "./user.entity";
+import { Tenant } from "./tenant.entity";
+
+export enum TaskStatus {
+    PENDENTE = "Pendente",
+    EM_PROGRESSO = "Em Progresso",
+    CONCLUIDA = "Concluída",
+}
+
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column()
+    title: string
+
+    @Column()
+    description: string
+
+    @Column({
+        type: "enum",
+        enum: TaskStatus,
+        default: TaskStatus.PENDENTE
+    })
+    status: TaskStatus
+
+    @Column()
+    deadline: Date
+
+    @ManyToOne(() => User, (user) => user.createdTasks)
+    createdBy: User
+
+    @ManyToOne(() => User, (user) => user.assignedTasks)
+    assignedTo: User
+
+    @ManyToOne(() => Tenant, (tenant) => tenant.tasks, { onDelete: "CASCADE" })
+    tenant: Tenant
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+}
