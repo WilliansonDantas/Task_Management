3fa3db8ad49f9e872cc35aaa8f8568d83d21b516
WilliansonDantas <williansondantas@gmail.com>
2024-09-28 18:18:22 -0300

    feat: create service for auth

---
+++ task-management-api/src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.usersService.findByEmail(email);
+        if (user && await bcrypt.compare(pass, user.password)) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async login(user: User) {
+        const payload = { username: user.username, sub: user.id, tenantId: user.tenant.id, role: user.role };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+
+}
