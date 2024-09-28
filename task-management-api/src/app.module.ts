ba78f49b46a6877d345ba4afff00bb234f50c138
WilliansonDantas <williansondantas@gmail.com>
2024-09-28 18:21:44 -0300

    refactor: add modules

--- task-management-api/src/app.module.ts
+++ task-management-api/src/app.module.ts
@@ -5,19 +5,30 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tenant } from './entities/tenant.entity';
 import { User } from './entities/user.entity';
 import { Task } from './entities/task.entity';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { TenantsModule } from './tenants/tenants.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'task_management',
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [Tenant, User, Task],
       synchronize: true,
-    })
+    }),
+    AuthModule,
+    TenantsModule,
+    UsersModule,
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
